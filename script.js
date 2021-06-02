c4c6d01c78e4ead621b3d7847dd5960d3cace7ef
KGK8 <63961693+KGK8@users.noreply.github.com>
2021-06-02 13:36:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const datepic = document.getElementById("date");
 const search = document.getElementById("search");
 const na = document.getElementById("name");
 const container = document.getElementById("tb");
+var today = new Date();
+console.log(today.toLocaleDateString("en-US"));
 
 const getAUserProfile = (pincod, date) => {
   var pin = pincod;
@@ -15,7 +17,7 @@ const getAUserProfile = (pincod, date) => {
   return fetch(api)
     .then((response) => {
       if (response.status !== 200) {
-        na.innerText = "Please Select Valied Pincode Or Date";
+        na.innerText = "Please Select Valied Pincode";
         // container.remove(newDiv);
       } else {
         na.innerText = " ";
@@ -36,9 +38,15 @@ const getAUserProfile = (pincod, date) => {
         const district_name = result.centers[x].district_name;
         const state_name = result.centers[x].state_name;
         const fee_ty = result.centers[x].fee_type;
-
+        const date = result.centers[x].sessions[0].date;
         const newDiv = document.createElement("tr");
         newDiv.classList.add("pannel");
+        //Date
+        const datespan = document.createElement("th");
+        datespan.classList.add("name");
+        newDiv.appendChild(datespan);
+        const date_vacc = document.createTextNode(date);
+        datespan.appendChild(date_vacc);
         //Name
         const namespan = document.createElement("th");
         namespan.classList.add("name");
@@ -112,37 +120,81 @@ const getAUserProfile = (pincod, date) => {
           newDiv.appendChild(agespan);
           const age_limit = document.createTextNode(m.available_capacity);
           agespan.appendChild(age_limit);
-          //FirstDose
-          const firstspan = document.createElement("th");
-          firstspan.classList.add("stat");
-          newDiv.appendChild(firstspan);
-          const available_capacity_dose1 = document.createTextNode(
-            m.available_capacity_dose1,
-          );
-          firstspan.appendChild(available_capacity_dose1);
-          //SecondDose
-          const secondspan = document.createElement("th");
-          secondspan.classList.add("stat");
-          newDiv.appendChild(secondspan);
-          const available_capacity_dose2 = document.createTextNode(
-            m.available_capacity_dose2,
-          );
-          secondspan.appendChild(available_capacity_dose2);
+          //   //FirstDose
+          //   const firstspan = document.createElement("th");
+          //   firstspan.classList.add("stat");
+          //   newDiv.appendChild(firstspan);
+          //   const available_capacity_dose1 = document.createTextNode(
+          //     m.available_capacity_dose1,
+          //   );
+          //   firstspan.appendChild(available_capacity_dose1);
+          //   //SecondDose
+          //   const secondspan = document.createElement("th");
+          //   secondspan.classList.add("stat");
+          //   newDiv.appendChild(secondspan);
+          //   const available_capacity_dose2 = document.createTextNode(
+          //     m.available_capacity_dose2,
+          //   );
+          //   secondspan.appendChild(available_capacity_dose2);
         }
         // });
         container.append(newDiv);
       }
     });
 };
-search.addEventListener("click", function () {
-  var pin = pincode.value;
-  if (pin === "") {
-    na.innerText = "Please Provide PinCode";
-    return;
+
+var auto_date = today.toISOString();
+var mod_auto_date = auto_date.split("-");
+var auto_year = mod_auto_date[0];
+var auto_month = mod_auto_date[1];
+var auto_day = mod_auto_date[2].split("");
+var mod_auto_date = auto_day[0] + auto_day[1];
+var final_auto_date = `${auto_year}-${auto_month}-${mod_auto_date}`;
+
+pincode.addEventListener("input", function () {
+  if (pincode.value.length === 6) {
+    console.log("change");
+    var pin = pincode.value;
+    if (pin === "") {
+      na.innerText = "Please Provide PinCode";
+      return;
+    }
+
+    var dat = datepic.value || final_auto_date;
+    if (dat === "") {
+      na.innerText = "Please Provide Date";
+    }
+    var date_spli = dat.split("-");
+    var modified_date = `${date_spli[2]}-${date_spli[1]}-${date_spli[0]}`;
+    console.log("Modified-Date:" + modified_date);
+    getAUserProfile(pin, modified_date);
+  } else {
+    na.innerText = "";
   }
-  var dat = datepic.value;
-  var date_spli = dat.split("-");
-  var modified_date = `${date_spli[2]}-${date_spli[1]}-${date_spli[0]}`;
-  console.log("Modified-Date:" + modified_date);
-  getAUserProfile(pin, modified_date);
 });
+
+datepic.addEventListener("input", function () {
+  if (pincode.value.length === 6) {
+    console.log("change");
+    var pin = pincode.value;
+    if (pin === "") {
+      na.innerText = "Please Provide PinCode";
+      return;
+    }
+
+    var dat = datepic.value; //|| today.toLocaleDateString("en-US");
+    console.log(dat);
+    if (dat === "") {
+      na.innerText = "Please Provide Date";
+    }
+    var date_spli = dat.split("-");
+    var modified_date = `${date_spli[2]}-${date_spli[1]}-${date_spli[0]}`;
+    console.log("Modified-Date:" + modified_date);
+    getAUserProfile(pin, modified_date);
+  }
+  else {
+    na.innerText = "";
+  }
+});
+
+console.log(final_auto_date);
